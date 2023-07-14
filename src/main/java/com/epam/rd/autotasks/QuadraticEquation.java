227340b7ad68cdf078c97b78ca6efc279851f864
Liubov Mitrofanova <mitrofanova.12113@gmail.com>
2023-07-14 15:05:12 +0300

    done

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import static java.lang.Math.sqrt;
 
 public class QuadraticEquation {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         double a = scanner.nextDouble();
