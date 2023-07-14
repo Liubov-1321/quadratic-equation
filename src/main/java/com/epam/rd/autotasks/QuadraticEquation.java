cc5ceb1ff8025c3e5bfaabf8141efc64d8c006ae
Liubov Mitrofanova <mitrofanova.12113@gmail.com>
2023-07-14 15:08:11 +0300

    done

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -16,11 +16,11 @@ public class QuadraticEquation {
         double D = (b * b - 4 * a * c);
         //System.out.println(D);
         if (D > 0) {
-        double x1 = (double) (-b - Math.sqrt(D) )/ (2 * a);
-        double x2 = (double) (-b + Math.sqrt(D) )/ (2 * a);
+        double x1 = (-b - Math.sqrt(D) )/ (2 * a);
+        double x2 = (-b + Math.sqrt(D) )/ (2 * a);
         System.out.println(x1 + " " + x2);
         } else if (D == 0) {
-        double x0 = (double) -b / (2 * a);
+        double x0 = ( -b / (2 * a));
         System.out.println(x0);
         } else {
             System.out.println("no roots");
